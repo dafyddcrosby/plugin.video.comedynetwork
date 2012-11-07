576e5ffb29e8c5ef5efc407a8c2922967c303a11
Dafydd Crosby <dtcrsby@gmail.com>
2012-11-07 00:52:30 -0700

    Now can get clip directory from episodes

--- default.py
+++ default.py
@@ -26,15 +26,27 @@ def list_seasons(show_id):
     return seasons
 
 def list_episodes(season_id):
-    """
-    TODO - figure out how we list the episodes
-    """
     url = "%s/VideoLibraryContents.aspx?GetChildOnly=true&SeasonID=%i" % \
         (AJAX_URL, season_id)
     scrape = urllib.urlopen(url)
-    return scrape
+    soup = BeautifulSoup.BeautifulSoup(scrape).findAll('a', id=True)
+    episodes = {}
+    for alink in soup:
+        episode_id = alink["id"].replace("Episode_", "")
+        episodes[alink.text] = int(episode_id)
+    return episodes
 
-
+def get_episode_clips(episode_id):
+    """
+    This is where it gets a bit tricky
+    """
+    url = "http://esi.ctv.ca/datafeed/content.aspx?cid=%i" % episode_id
+    scrape = urllib.urlopen(url)
+    soup = BeautifulSoup.BeautifulStoneSoup(scrape).find('playlist').findAll('element')
+    clips = {}
+    for clip in soup:
+        clips[clip.title.contents[0].strip()] = int(clip["id"])
+    return clips
 
 def main():
     pass
